6409b294faf98e3bf3c57423c7b6072cbcc9eb34
Naveen Valecha <naveen.valecha@morpht.com>
2024-01-29 08:30:17 +0000

    convivial-demo-35155: Cypress: Basics - PHP memory_limit.
    Mirrored from (convivial-demo-35155).

--- .github/scripts/setup_app.sh
+++ .github/scripts/setup_app.sh
@@ -5,7 +5,7 @@ set -ev
 export PATH=${COMPOSER_BIN}:$PATH
 
 # Up the PHP Memory Limit
-sudo echo 'memory_limit = -1' >> /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
+# echo 'memory_limit = -1' >> /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
 
 # Create a MySQL database for drupal to use
 MYSQL_ROOT_COMMAND="mysql --user=root --password=$MYSQL_ROOT_PASSWORD --host=$MYSQL_HOST --protocol=tcp"
